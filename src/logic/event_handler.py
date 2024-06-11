05a8328f7c56d713c19711a0c4a2ae67d1cb6a9a
DaHL <8tima18@gmail.com>
2024-06-12 01:40:06 +0500

    mouse control moved to event_handler.py

--- src/logic/event_handler.py
+++ src/logic/event_handler.py
@@ -3,8 +3,17 @@ import sys
 
 
 class EventHandler:
+    mouse_data = {1: 'left', 2: 'middle', 3: 'right'}
+
     def __init__(self, window):
         self.window = window
+
+        # MOUSE
+        self.drag_info = None
+        self.last_press = dict((key, dict((key, {'pos': (0, 0), 'time': 0}) for key in ('single', 'double')))
+                               for key in self.mouse_data.values())
+
+        self.last_press = {'b_name': '', 'pos': (0, 0), 'time': 0, 'count': 0}
 
     def handle_events(self):
         window = self.window
@@ -21,8 +30,7 @@ class EventHandler:
 
                 window.ctx.viewport = (0, 0) + window.size
 
-                for widget in gui.widgets:
-                    widget.update_vertices()
+                gui.size = window.size
 
             elif event.type == pg.QUIT:
                 pg.quit()
@@ -48,16 +56,59 @@ class EventHandler:
 # //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
             elif event.type == pg.MOUSEBUTTONDOWN:
-                if event.dict['button'] in gui.mouse_data:
-                    button_name = gui.mouse_data[event.dict['button']]
+                if event.dict['button'] in self.mouse_data:
+                    button_name = self.mouse_data[event.dict['button']]
 
-                    gui.mouse_down(button_name, pg.mouse.get_pos())
+                    current_time = pg.time.get_ticks()
+
+                    if self.last_press['b_name'] == button_name:
+                        time_from_last_click = current_time - self.last_press['time']
+                        if_click_in_range = any(abs(self.last_press['pos'][i] - event.dict['pos'][i]) < 2 for i in (0, 1))
+
+                        if if_click_in_range and time_from_last_click < 500:
+                            self.last_press['count'] += 1
+                        else:
+                            self.last_press['count'] = 1
+
+                    else:
+                        self.last_press['b_name'] = button_name
+                        self.last_press['count'] = 1
+
+                    self.last_press['time'] = current_time
+                    self.last_press['pos'] = event.dict['pos']
+
+                    for widget in gui.widgets:
+                        if widget.cords_in_rect(event.dict['pos']):
+                            widget.mouse_down(button_name, event.dict['pos'], self.last_press['count'])
 
             elif event.type == pg.MOUSEBUTTONUP:
-                if event.dict['button'] in gui.mouse_data:
-                    button_name = gui.mouse_data[event.dict['button']]
+                if event.dict['button'] in self.mouse_data:
+                    button_name = self.mouse_data[event.dict['button']]
 
-                    gui.mouse_up(button_name, pg.mouse.get_pos())
+                    for widget in gui.widgets:
+                        if widget.cords_in_rect(event.dict['pos']):
+                            widget.mouse_up(button_name, event.dict['pos'])
+
+                    if self.drag_info is not None and self.drag_info['b_name'] == button_name:
+                        self.drag_info = None
 
             elif event.type == pg.MOUSEMOTION:
-                gui.mouse_motion(event.dict['buttons'], event.dict['pos'], event.dict['rel'])
+                if_click_out_of_range = any(abs(self.last_press['pos'][i] - event.dict['pos'][i]) > 2 for i in (0, 1))
+
+                pressed_buttons = set(self.mouse_data[i + 1] if event.dict['buttons'][i] else '' for i in range(3))
+
+                # DRAGGING
+                if self.drag_info is not None:
+                    self.drag_info['widget'].mouse_drag(self.drag_info['b_name'], event.dict['pos'], event.dict['rel'])
+
+                elif if_click_out_of_range and self.last_press['b_name'] in pressed_buttons:
+                    for i in range(len(gui.widgets)):
+                        w = gui.widgets[i]
+                        if w.cords_in_rect(self.last_press['pos']):
+                            self.drag_info = {'b_name': self.last_press['b_name'], 'widget': w}
+                            gui.widgets.pop(i)
+                            gui.widgets.insert(0, w)
+
+                            w.mouse_drag(self.last_press['b_name'], event.dict['pos'], event.dict['rel'])
+
+                            break
